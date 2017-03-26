347517a22eb2882cdd078f73ae55572ed91ed2e8
Zattri <daninjakidy@gmail.com>
2017-03-26 10:52:24 +0100

    Added the functions to the script, need testing

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 function getPhoto() {
-  //return a random photo from the /images directory
+  // Return a file name string for image
+  var images = [
+      "/images/1.jpg",
+      "/images/2.jpg",
+      "/images/3.jpg",
+      "/images/4.jpg",
+      "",
+
+  ]
+
+  var randomPhoto = images[Math.floor(Math.random() * images.length)];
+  console.log(randomPhoto);
+  return randomPhoto;
 }
 
 function getCaption() {
-  //return a random caption
+  // Return a random caption - string
+  var quotes = [
+      "Ballin' on a budget",
+      "<insert name here> for MVP",
+      "Looking forward to <insert hackathon here> this weekend",
+      "My drone's so heavy",
+      "You wanna coem see my drone?",
+      "I should probably take some pictures",
+      "I'm the official MLH photographer",
+  ]
+  var randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+  console.log(randomQuote);
+  return randomQuote;
 }
