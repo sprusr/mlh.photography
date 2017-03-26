025a38ab22bbb0ba72cfaf754d41bde3c089e32a
Zattri <daninjakidy@gmail.com>
2017-03-26 11:37:29 +0100

    Shuffle array works, implemented

--- script.js
+++ script.js
@@ -1,36 +1,74 @@
 var CHANGE_INTERVAL = 8000;
+var currPhoto = 0;
+var currQuote = 0;
+
+var images = [
+    "images/1.jpg",
+    "images/2.jpg",
+    "images/3.jpg",
+    "images/4.jpg",
+    "images/5.jpg",
+    "images/6.jpg",
+    "images/7.jpg",
+    "images/8.jpg",
+]
+
+var quotes = [
+    "Ballin' on a budget",
+    "<insert name here> for MVP",
+    "Looking forward to <insert hackathon here> this weekend",
+    "My drone's so heavy",
+    "You wanna come see my drone?",
+    "I should probably take some pictures",
+    "I'm the official MLH photographer",
+    "Do you want to go to the pub later?",
+    "#dronephotography",
+    "#ballin",
+]
 
 function getPhoto() {
   // Return a file name string for image
-  var images = [
-      "images/1.jpg",
-      "images/2.jpg",
-      "images/3.jpg",
-      "images/4.jpg"
-  ]
-  var randomPhoto = images[Math.floor(Math.random() * images.length)];
-  console.log(randomPhoto);
-  document.getElementById("image").src=randomPhoto;
+  var randomPhoto = images[currPhoto];
+  if (currPhoto < images.length) {
+      currPhoto++;
+  }
+  else {
+      images = shuffle(images);
+      currPhoto = 0;
+  }
   return randomPhoto;
 }
 
 function getCaption() {
   // Return a random caption - string
-  var quotes = [
-      "Ballin' on a budget",
-      "<insert name here> for MVP",
-      "Looking forward to <insert hackathon here> this weekend",
-      "My drone's so heavy",
-      "You wanna coem see my drone?",
-      "I should probably take some pictures",
-      "I'm the official MLH photographer",
-      "Do you want to go to the pub later?",
-      "#dronephotography",
-      "#ballin",
-  ]
-  var randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
-  console.log(randomQuote);
+  var randomQuote = quotes[currQuote];
+  if (currQuote < images.length) {
+      currQuote++;
+  }
+  else {
+      quotes = shuffle(quotes);
+      currQuote = 0;
+  }
   return randomQuote;
+}
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
 
 function setPhotoAndQuote() {
@@ -44,3 +82,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
     setPhotoAndQuote();
   }, CHANGE_INTERVAL);
 });
+
+images = shuffle(images);
+quotes = shuffle(quotes);
