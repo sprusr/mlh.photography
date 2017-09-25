d1031cc0346f3aecc3e136f4ffcc6f2e1291d44b
Matt Bell <logicnotlogical@outlook.com>
2017-09-25 12:10:36 +0100

    Add new photo (#5)

--- script.js
+++ script.js
@@ -3,44 +3,44 @@ var currPhoto = 0;
 var currQuote = 0;
 
 var images = [
-    "images/1.jpg",
-    "images/2.jpg",
-    "images/3.jpg",
-    "images/4.jpg",
-    "images/5.jpg",
-    "images/6.jpg",
-    "images/7.jpg",
-    "images/8.jpg",
-    "images/9.jpg",
-    "images/10.jpg",
-    "images/11.jpg",
-    "images/12.jpg"
-]
+  "images/1.jpg",
+  "images/2.jpg",
+  "images/3.jpg",
+  "images/4.jpg",
+  "images/5.jpg",
+  "images/6.jpg",
+  "images/7.jpg",
+  "images/8.jpg",
+  "images/9.jpg",
+  "images/10.jpg",
+  "images/11.jpg",
+  "images/12.jpg",
+  "images/13.jpg",
+];
 
 var quotes = [
-    "Ballin' on a budget",
-    "&lt;insert name here&gt; for MVP",
-    "Looking forward to &lt;insert hackathon here&gt this weekend",
-    "My drone's so heavy",
-    "You wanna come see my drone?",
-    "I should probably take some pictures",
-    "I'm the official MLH photographer",
-    "Do you want to go to the pub later?",
-    "#dronephotography",
-    "#ballin",
-    "I'm hacking at a hackathon",
-    "MLH money",
-]
+  "Ballin' on a budget",
+  "&lt;insert name here&gt; for MVP",
+  "Looking forward to &lt;insert hackathon here&gt this weekend",
+  "My drone's so heavy",
+  "You wanna come see my drone?",
+  "I should probably take some pictures",
+  "I'm the official MLH photographer",
+  "Do you want to go to the pub later?",
+  "#dronephotography",
+  "#ballin",
+  "I'm hacking at a hackathon",
+  "MLH money",
+];
 
 function getPhoto() {
   // Return a file name string for image
   var randomPhoto = images[currPhoto];
-  if (currPhoto < images.length-1) {
-      currPhoto++;
-  }
-  else {
-      images = shuffle(images);
-      currPhoto = 0;
+  if (currPhoto < images.length - 1) {
+    currPhoto++;
+  } else {
+    images = shuffle(images);
+    currPhoto = 0;
   }
   return randomPhoto;
 }
@@ -48,22 +48,22 @@ function getPhoto() {
 function getCaption() {
   // Return a random caption - string
   var randomQuote = quotes[currQuote];
-  if (currQuote < images.length-1) {
-      currQuote++;
-  }
-  else {
-      quotes = shuffle(quotes);
-      currQuote = 0;
+  if (currQuote < images.length - 1) {
+    currQuote++;
+  } else {
+    quotes = shuffle(quotes);
+    currQuote = 0;
   }
   return randomQuote;
 }
 
 function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
   // While there remain elements to shuffle...
   while (0 !== currentIndex) {
-
     // Pick a remaining element...
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
@@ -78,14 +78,17 @@ function shuffle(array) {
 }
 
 function setPhotoAndQuote() {
-  document.getElementById("image").style.backgroundImage = "url('" + getPhoto() + "')";
-  document.getElementById("caption").innerHTML = "\"" + getCaption() + "\"";
+  document.getElementById("image").style.backgroundImage =
+    "url('" + getPhoto() + "')";
+  document.getElementById("caption").innerHTML = '"' + getCaption() + '"';
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
   setPhotoAndQuote();
-  document.getElementById("image").style.animation = "imagescroll " + (CHANGE_INTERVAL/1000) + "s infinite linear";
-  document.getElementById("caption").style.animation = "captionfade " + (CHANGE_INTERVAL/1000) + "s infinite linear";
+  document.getElementById("image").style.animation =
+    "imagescroll " + CHANGE_INTERVAL / 1000 + "s infinite linear";
+  document.getElementById("caption").style.animation =
+    "captionfade " + CHANGE_INTERVAL / 1000 + "s infinite linear";
   setInterval(function() {
     setPhotoAndQuote();
   }, CHANGE_INTERVAL);
